85c40e27b011391475945c7485ca639a3af12eed
Vladyslav <vladislav.sel@gmail.com>
2018-09-06 17:03:10 +0100

    Hotfix for current version to fix Firefox click issue

--- src/components/DayColumnItem/index.js
+++ src/components/DayColumnItem/index.js
@@ -12,11 +12,11 @@ class DayColumnItem extends Component {
 			);
 		}
 		return (
-			<button className={"button-no-decoration day-column__section " + (this.props.selectedDayTask ? 'day-column__section--active ' : '') + (this.props.hoverId === this.props.item.id ? 'day-column__section--hovered' : '')}>
+			<div className={"button-no-decoration day-column__section " + (this.props.selectedDayTask ? 'day-column__section--active ' : '') + (this.props.hoverId === this.props.item.id ? 'day-column__section--hovered' : '')} tabindex="0">
 				<Icon onClick={() => this.props.addItem(this.props.day, this.props.item.id)} className="day-column__init-icon day-column__init-icon--plus" type="plus-circle-o" />
 				<Icon onClick={() => this.props.addItem(this.props.day, this.props.item.id)} className="day-column__init-icon day-column__init-icon--minus" type="minus-circle-o" />
 				{radio}
-			</button>
+			</div>
 		);
 	}
 }
